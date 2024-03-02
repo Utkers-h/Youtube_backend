06c1fcfbd0063b3c93c58288a5e3518daee9929c
Utkersh uppal <utkersh2003uppal@gmail.com>
2024-03-03 00:08:11 +0530

    Added APiErros

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,9 @@
-// const asyncHandler = () =>{
+const asyncHandler = (requestHandler) =>{
+    (req, res , next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
 
-// }
+}
 
 export {asyncHandler}
 
