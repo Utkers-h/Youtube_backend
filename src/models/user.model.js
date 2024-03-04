78c6091b4638aba0c8f42e4c29b2eff64c6accd1
Utkersh uppal <utkersh2003uppal@gmail.com>
2024-03-05 00:24:29 +0530

    Added cloudinary and multer functionalities

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt  from 'bcrypt';
+import bcrypt from 'bcrypt';
 
 // whenever we want a field to be searched frequently , make its index true, it will optimize the search results
 
@@ -51,42 +51,42 @@ const userSchema = new Schema({
     timestamps: true
 })
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function (next) {
     // to prevent the middleware to always execute whenever user changes its profile , we only check for the password field
 
-    if(!this.isModified("password")) return next();
+    if (!this.isModified("password")) return next();
 
-    this.password =  bcrypt.hash(this.password, 10)
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken =  function(){
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email,
         username: this.username,
         fullname: this.fullname
     },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
     )
 }
 
-userSchema.methods.generateRefreshToken= function(){
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
-        
+
     },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-    }
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
     )
 
 }
